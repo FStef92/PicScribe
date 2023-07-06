32083931eeae3627d8c8bf72f8a219a1acd5c827
stefanifl <francesco.stefanimv@gmail.com>
2023-07-07 00:34:12 +0200

    changing to camerax

--- app/src/main/java/com/fs/picscribe/MainActivity.kt
+++ app/src/main/java/com/fs/picscribe/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fs.picscribe
 
 import android.Manifest
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -12,6 +13,7 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.Surface.ROTATION_0
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,7 +24,10 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.compose.foundation.layout.Column
+import androidx.camera.view.PreviewView
+import androidx.camera.view.PreviewView.ImplementationMode
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
@@ -30,7 +35,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.fs.picscribe.ui.theme.PicScribeTheme
 import java.io.File
@@ -42,43 +51,59 @@ class MainActivity : ComponentActivity()
 {
     private val TAG: String = "PLCSCRIBE"
     private lateinit var imageCapture: ImageCapture
+    private lateinit var preview: Preview
     private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
 
-
-        imageCapture = ImageCapture.Builder()
-            .setTargetRotation(ROTATION_0)
-            .build()
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-cameraProviderFuture.addListener( {
-    val cameraProvider = cameraProviderFuture.get()
-    val preview = Preview.Builder()
-        .build()
-
-    val cameraSelector = CameraSelector.Builder()
-        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-        .build()
-    cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture)
-    // Your code using cameraProvider goes here
-}, ContextCompat.getMainExecutor(this))
-
-
+        preview = Preview.Builder().build()
 
         setContent {
             PicScribeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        Greeting("Android")
-                        CameraButton()
-                    }
+                    CameraScreen()
                 }
+
             }
         }
+
+
+        val rotation = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+        {
+            this.display?.rotation ?: ROTATION_0
+        } else
+        {
+            val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            @Suppress("DEPRECATION")
+            windowManager.defaultDisplay.rotation
+        }
+
+
+
+        imageCapture = ImageCapture.Builder()
+            .setTargetRotation(rotation)
+            .build()
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            // Your code using cameraProvider goes here
+        }, ContextCompat.getMainExecutor(this))
+
+
         cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK)
             {
@@ -93,12 +118,40 @@ cameraProviderFuture.addListener( {
     }
 
     @Composable
+    private fun CameraScreen()
+    {
+
+        val context = LocalContext.current
+        val previewView = remember {
+            PreviewView(context)
+        }
+
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+            modifier = Modifier.fillMaxSize()
+        ){
+            AndroidView(
+                factory = { previewView },
+                modifier = Modifier.fillMaxWidth().fillMaxHeight()
+            )
+            {
+                preview.setSurfaceProvider(previewView.surfaceProvider)
+            }
+            CameraButton()
+        }
+
+
+    }
+
+
+
+    @Composable
     fun CameraButton()
     {
         Button(
             onClick = {
                 checkPermissionsAndLaunch()
-            }, modifier = Modifier.fillMaxWidth()
+            }, modifier = Modifier.fillMaxWidth(0.4f)
         ) {
             Text(text = "Capture Photo")
         }
@@ -112,7 +165,7 @@ cameraProviderFuture.addListener( {
 
         val permissionsToRequest = mutableListOf<String>()
 
-        val hasCameraPermission= ContextCompat.checkSelfPermission(
+        val hasCameraPermission = ContextCompat.checkSelfPermission(
             this@MainActivity, Manifest.permission.CAMERA
         ) == PackageManager.PERMISSION_GRANTED
         if (!hasCameraPermission)
@@ -181,96 +234,88 @@ cameraProviderFuture.addListener( {
             "HHmmss"
         }
         val filename = currentTime + "_PicScribe.heic"
+        val folder = "PicScribe"
+        val projectName = ""//100_10_1 Fine"
 
         val values = ContentValues()
+
         values.put(MediaStore.MediaColumns.TITLE, filename)
         values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
         values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000)
         values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
-        values.put(MediaStore.MediaColumns.MIME_TYPE, "image/heic")
-        values.put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/PicScribe")
 
-        val outPhotoUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI //.buildUpon().appendPath("PicScribe").build()
+        createFileUri(folder, projectName, filename)?.let {
+            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(
+                contentResolver,
+                it,
+                values
+            ).build()
 
-
-        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(
-            contentResolver,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            values).build()
-
-        imageCapture.takePicture(outputFileOptions, Executors.newSingleThreadExecutor(),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(error: ImageCaptureException)
+            imageCapture.takePicture(outputFileOptions, Executors.newSingleThreadExecutor(),
+                object : ImageCapture.OnImageSavedCallback
                 {
-                    Log.d(TAG, "onError: ")// insert your code here.
-                    error.printStackTrace()
-                }
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    Log.d(TAG, "onImageSaved: ")// insert your code here.
-                }
-            })
+                    override fun onError(error: ImageCaptureException)
+                    {
+                        Log.d(TAG, "onError: ")// insert your code here.
+                        error.printStackTrace()
+                    }
+
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults)
+                    {
+                        Log.d(TAG, "onImageSaved: ")// insert your code here.
+                    }
+                })
+
+        }
+
+
+
+
 
     }
 
     private lateinit var currentPhotoPath: String
 
 
-    private fun createFolderUri(directoryName: String, filename: String): Uri {
+    private fun createFileUri(appName: String, directoryName: String, filename: String): Uri?
+    {
+        var retFile: Uri? = null
 
+        if (Build.VERSION.SDK_INT >= 29)
+        {
+            val values = ContentValues()
+            values.put(MediaStore.MediaColumns.TITLE, filename)
+            values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+            values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000)
+            values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+            values.put(MediaStore.MediaColumns.MIME_TYPE, "image/heic")
+            values.put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/$appName")///$directoryName")
 
-        val values = ContentValues()
-        //values.put(MediaStore.MediaColumns.TITLE, filename)
-        //values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
-        //values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000)
-
-        var retFile: Uri?
-        val newUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI.buildUpon().appendPath("PicScribe").build()
-        val directory = File(newUri.toString())
-        directory.mkdirs()
-        retFile = newUri
-        return retFile
-        if (Build.VERSION.SDK_INT >= 29) {
-            values.put(MediaStore.MediaColumns.RELATIVE_PATH, "Pictures/$directoryName")
-            //values.put(MediaStore.MediaColumns.DATE_TAKEN, System.currentTimeMillis())
             values.put(MediaStore.MediaColumns.IS_PENDING, true)
 
-            //contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
             val uri: Uri? = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-            if (uri != null) {
-                //saveImageToStream(bitmap, context.contentResolver.openOutputStream(uri))
+            if (uri != null)
+            {
                 values.put(MediaStore.Images.Media.IS_PENDING, false)
                 contentResolver.update(uri, values, null, null)
                 retFile = uri
             }
 
-        } else {
-            val directory = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), directoryName)
+        } else
+        {
+            val directory: File = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), directoryName)
             directory.mkdirs()
             retFile = Uri.fromFile(directory)
         }
 
-        return retFile!!
+        return retFile
     }
-    override fun onDestroy() {
+
+    override fun onDestroy()
+    {
         super.onDestroy()
         cameraLauncher.unregister()
     }
 }
 
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier)
-{
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
-
-@androidx.compose.ui.tooling.preview.Preview(showBackground = true)
-@Composable
-fun GreetingPreview()
-{
-    PicScribeTheme {
-        Greeting("Android")
-    }
-}
